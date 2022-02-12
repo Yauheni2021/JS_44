88f20229178cf1684ff2ab600518bef9450b24df
Yauheni Kaspiarovich <kaspiarovichy@gmail.com>
2022-02-12 10:34:49 +0100

    test

---
+++ js/test.js
@@ -0,0 +1,25 @@
+// const max = 10;
+// for (let i = 0; i < max; i += 1) {
+//     console.log(`${max} % $ {i}=`, max % i);
+// }
+
+// for (let i = 0; i <= 5; i += 1) {
+//   console.log(i);
+
+//   if (i === 3) {
+//     console.log("Нашли число 3, прерываем выполнение цикла");
+//     break;
+//   }
+// }
+
+// console.log("Лог после цикла");
+
+// const number = 10;
+
+// for (let i = 0; i < number; i += 1) {
+//   if (i % 2 === 0) {
+//     continue;
+//   }
+
+//   console.log("Нечетное i: ", i); // 1, 3, 5, 7, 9
+// }
